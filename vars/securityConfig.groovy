3a6610467dc5d91829d5fa2793f06f695e4ede06
Sagar Patel <49648512+psagar-dev@users.noreply.github.com>
2025-05-14 08:51:53 +0530

    Update securityConfig.groovy

--- vars/securityConfig.groovy
+++ vars/securityConfig.groovy
@@ -1,7 +1,7 @@
-def call(String dockerImage = 'securelooper/flask-cicd:latest', String dockerContainerName = "flask-cicd-container", String DockerCredentalsId = "docker-hub-creds") {
+def call(String dockerImage = 'securelooper/flask-cicd:latest', String dockerContainerName = "flask-cicd-container", String dockerCredentalsId = "docker-hub-creds") {
     return [
         DOCKER_IMAGE: dockerImage,
         CONTAINER_NAME: dockerContainerName,
-        DOCKER_CREDENTIALS_ID: DockerCredentalsId
+        DOCKER_CREDENTIALS_ID: dockerCredentalsId
     ]
 }
