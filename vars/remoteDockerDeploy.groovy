2c0d95995a884bb59e0d9e7a274350f1ab43d2d2
Sagar Patel <49648512+psagar-dev@users.noreply.github.com>
2025-05-14 09:08:41 +0530

    Update remoteDockerDeploy.groovy

--- vars/remoteDockerDeploy.groovy
+++ vars/remoteDockerDeploy.groovy
@@ -2,17 +2,11 @@ def call(String dockerImage, String dockerContainerName, String ec2User, String 
     sshagent(credentials: ["${sshId}"]) {
         sh """
             ssh -o StrictHostKeyChecking=no ${ec2User}@${ec2Host} '
-            echo "Pulling latest Docker image"
             sudo docker pull ${dockerImage}
 
-            echo "Stopping any existing container..."
             sudo docker stop ${dockerContainerName} || true
             sudo docker rm ${dockerContainerName} || true
-
-            echo "Running the container..."
             sudo docker run -d --name ${dockerContainerName} -p ${ports} ${dockerImage}
-
-            echo "Deployment successful!"
             '
         """
     }
